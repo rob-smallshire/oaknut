835accb852bfb1e918742b9f573edb6f09d9a676
Robert Smallshire <robert@smallshire.org.uk>
2026-05-25 15:02:09 +0200

    Bump oaknut to 11.0.1

--- packages/oaknut-discimage/src/oaknut/discimage/__init__.py
+++ packages/oaknut-discimage/src/oaknut/discimage/__init__.py
@@ -9,7 +9,7 @@ generic ``DiscFormat`` dataclass. Filesystem-specific constants
 in its own package, not here.
 """
 
-__version__ = "11.0.0"
+__version__ = "11.0.1"
 
 from oaknut.discimage.formats import (
     BYTES_PER_SECTOR,
--- packages/oaknut-exception/src/oaknut/exception/__init__.py
+++ packages/oaknut-exception/src/oaknut/exception/__init__.py
@@ -38,7 +38,7 @@ from typing import Iterator, Optional
 
 from exit_codes import ExitCode
 
-__version__ = "11.0.0"
+__version__ = "11.0.1"
 
 __all__ = [
     "OaknutException",
