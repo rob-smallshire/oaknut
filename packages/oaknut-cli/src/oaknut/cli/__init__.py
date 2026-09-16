dc55494fd26429d7cc818668d790ff753d6f311c
Robert Smallshire <robert@smallshire.org.uk>
2026-09-16 22:27:21 +0200

    Bump oaknut to 12.18.1

--- packages/oaknut-cli/src/oaknut/cli/__init__.py
+++ packages/oaknut-cli/src/oaknut/cli/__init__.py
@@ -40,7 +40,7 @@ from oaknut.cli.reports import (
     text_cell,
 )
 
-__version__ = "12.18.0"
+__version__ = "12.18.1"
 
 __all__ = [
     "COMMAND_KIND",
--- packages/oaknut-disc/src/oaknut/disc/__init__.py
+++ packages/oaknut-disc/src/oaknut/disc/__init__.py
@@ -1,6 +1,6 @@
 """Unified CLI for Acorn DFS, ADFS, and AFS disc images."""
 
-__version__ = "12.18.0"
+__version__ = "12.18.1"
 
 from oaknut.disc.cli import gather  # noqa: E402  (needs __version__ defined first)
 
